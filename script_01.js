9d33e4de8ee4431015b62b89a23117a8e13c59dc
ziniminisoldier <philipp.karst@gmx.de>
2023-05-10 10:46:23 +0200

    CF / ternary

--- script_01.js
+++ script_01.js
@@ -3,7 +3,7 @@
 /*Entscheidungsstrukturen | control flow */
 
 // Deklaration + Assignment
-const ageJohn = 25;
+const ageJohn = 30;
 const ageMark = 30;
 
 // Deklaration
@@ -29,7 +29,26 @@ console.log("------------------");
 // if(true)
 // if(false)
 // if(isJohnOlder)
-if(ageJohn > ageMark)
-{
-console.log("John ist älter. ");
-}
+// if(ageJohn > ageMark)
+// {
+// console.log("John ist älter. ");
+// }
+
+/************ IF - ELSE  ************/
+// mit Alternative  
+// entweder ja oder nein
+
+// if(true)
+// if(false)
+// if(isJohnOlder)
+// if(ageJohn > ageMark)
+// {
+// console.log("John ist älter. ");
+// }
+// else
+// {
+// console.log("John ist jünger. ");
+// }
+
+/************  Ternäre (ternary) Schreibweise ************/
+// console.log( (isJohnOlder) ? "John is older" : "Mark is older");
